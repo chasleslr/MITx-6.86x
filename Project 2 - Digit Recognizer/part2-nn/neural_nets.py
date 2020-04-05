bca7bda2cac955755ea110149d15883484d9acf7
chasleslr <charles@dribbble.com>
2020-04-05 13:36:27 -0400

    Create rectified_linear_unit

--- Project 2 - Digit Recognizer/part2-nn/neural_nets.py
+++ Project 2 - Digit Recognizer/part2-nn/neural_nets.py
@@ -15,7 +15,8 @@ import math
 
 def rectified_linear_unit(x):
     """ Returns the ReLU of x, or the maximum between 0 and x."""
-    # TODO
+    return max(0,x)
+
 
 def rectified_linear_unit_derivative(x):
     """ Returns the derivative of ReLU."""

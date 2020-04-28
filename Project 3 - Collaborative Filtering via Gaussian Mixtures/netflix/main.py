826bd33895b498ffd7d9d0daf0d427ec4b45e2c6
chasleslr <charles@dribbble.com>
2020-04-28 17:54:48 -0400

    Implemented k-means and naive-em

--- Project 3 - Collaborative Filtering via Gaussian Mixtures/netflix/main.py
+++ Project 3 - Collaborative Filtering via Gaussian Mixtures/netflix/main.py
@@ -90,3 +90,42 @@ print(f"Best K={best_K}", f"BIC={best_bic}")
 # -----------------------------------
 
 print('\n Expectation-Maximization Algorithm for Matrix Completion')
+
+X = np.loadtxt("netflix_incomplete.txt")
+
+seeds = [0, 1, 2, 3, 4]
+K = [1, 12]
+bic = np.zeros(len(K))
+
+for j, k in enumerate(K):
+    mixtures = []
+    posts = []
+    logloss = np.empty(len(seeds))
+
+    for i, seed in enumerate(seeds):
+        # initialize mixture model with random points
+        mixture, post = common.init(X, K=k, seed=seed)
+
+        # run EM-algorithm
+        mixture, post, LL = em.run(X, mixture=mixture, post=post)
+
+        mixtures.append(mixture)
+        posts.append(post)
+        logloss[i] = LL
+        print('K=', k, 'seed=', seed, 'logloss=', LL)
+
+    best_seed = np.argmax(logloss)
+    logloss = logloss[best_seed]
+    mixture = mixtures[best_seed]
+    post = posts[best_seed]
+
+    current_bic = common.bic(X, mixture, logloss)
+    bic[j] = current_bic
+
+    print(f'K={k}', f'Best seed={best_seed}', f'logloss={logloss}', f'BIC={current_bic}')
+    #common.plot(X, mixture, post, title=f"EM, K={k}")
+
+best_K_ix = np.argmax(bic)
+best_K = K[best_K_ix]
+best_bic = bic[best_K_ix]
+print(f"Best K={best_K}", f"BIC={best_bic}")
